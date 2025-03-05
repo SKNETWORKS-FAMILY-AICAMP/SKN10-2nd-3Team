73c83fb8f261120cd3419e16a2b198fbd1768e49
redwin-02 <tmdvy2572@gmail.com>
2025-03-05 12:30:38 +0900

    스크린샷 업로드드

--- landing.py
+++ landing.py
@@ -4,24 +4,47 @@ import streamlit as st
 st.title("🎨 탈주는 아니될 말이오")
 
 # 셀렉트 박스 (옵션 리스트)
-option = st.selectbox("옵션을 선택하세요", ["LightGBM", "XGBoost", "KNN", "RandomForest", "LogisticRegression", "앙상블"])
+option = st.selectbox("옵션을 선택하세요", ["LightGBM", "XGBoost", "RandomForest", "LogisticRegression"])
 
 # 선택한 옵션에 따라 이미지 표시
 if option == "LightGBM":
-    st.image("dog1.jpg", caption="강아지 1")
-    st.image("dog2.jpg", caption="강아지 2")
+    st.image("fucking/accuracy.png", caption="고양이 1")
+    st.image("fucking/confusion_matrix.png", caption="고양이 2")
+    st.image("fucking/f1_score.png", caption="고양이 2")
+    st.image("fucking/feature_importance.png", caption="고양이 1")
+    st.image("fucking/log_loss.png", caption="고양이 2")
+    st.image("fucking/precision.png", caption="고양이 1")
+    st.image("fucking/recall.png", caption="고양이 2")
+    st.image("fucking/roc_auc.png", caption="고양이 2")
+
 elif option == "XGBoost":
-    st.image("cat1.jpg", caption="고양이 1")
-    st.image("cat2.jpg", caption="고양이 2")
-elif option == "KNN":
-    st.image("nature1.jpg", caption="자연 1")
-    st.image("nature2.jpg", caption="자연 2")
+    st.image("ye/accuracy.png", caption="고양이 1")
+    st.image("ye/confusion_matrix.png", caption="고양이 2")
+    st.image("ye/f1_score.png", caption="고양이 2")
+    st.image("ye/feature_importance.png", caption="고양이 1")
+    st.image("ye/log_loss.png", caption="고양이 2")
+    st.image("ye/metrics.png", caption="고양이 2")
+    st.image("ye/precision.png", caption="고양이 1")
+    st.image("ye/recall.png", caption="고양이 2")
+    st.image("ye/roc_auc_curve.png", caption="고양이 2")
+
 elif option == "RandomForest":
-    st.image("cat1.jpg", caption="고양이 1")
-    st.image("cat2.jpg", caption="고양이 2")
+    st.image("jungwoo/accuracy.png", caption="고양이 1")
+    st.image("jungwoo/confusion_matrix.png", caption="고양이 2")
+    st.image("jungwoo/epoch.png", caption="고양이 1")
+    st.image("jungwoo/f1_score.png", caption="고양이 2")
+    st.image("jungwoo/feature_importance.png", caption="고양이 1")
+    st.image("jungwoo/log_loss.png", caption="고양이 2")
+    st.image("jungwoo/precision.png", caption="고양이 1")
+    st.image("jungwoo/recall.png", caption="고양이 2")
+    st.image("jungwoo/roc_auc.png", caption="고양이 2")
+
 elif option == "LogisticRegression":
-    st.image("nature1.jpg", caption="자연 1")
-    st.image("nature2.jpg", caption="자연 2")
-elif option == "앙상블":
-    st.image("nature1.jpg", caption="자연 1")
-    st.image("nature2.jpg", caption="자연 2")
+    st.image("jungwoo2/accuracy.png", caption="고양이 1")
+    st.image("jungwoo2/confusion matrix.png", caption="고양이 2")
+    st.image("jungwoo2/epoch.png", caption="고양이 1")
+    st.image("jungwoo2/f1_score.png", caption="고양이 2")
+    st.image("jungwoo2/log_loss.png", caption="고양이 2")
+    st.image("jungwoo2/precision.png", caption="고양이 1")
+    st.image("jungwoo2/recall.png", caption="고양이 2")
+    st.image("jungwoo2/roc_auc.png", caption="고양이 2")
